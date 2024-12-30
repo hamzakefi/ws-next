ca4e076f72b48366a5f8d8cfa2dbb169cebdb024
hamzakefi <kefihamza23.hamza@gmail.com>
2024-12-30 20:18:58 +0100

    haha

---
+++ app/products/page.js
@@ -0,0 +1,11 @@
+export default function ProductPage() {
+	return (
+		<div className='products'>
+			<h2>This is the Product page</h2>
+			<img
+				src="https://sommersadvisors.com/wp-content/uploads/2021/05/tuto-vinted-comment-vendre-en-ligne-vinted-tutorial-produit-article-vetement.png" 
+				style={{ width: '500px', height: '300px' }} 
+			/>
+		</div>
+	);
+}
